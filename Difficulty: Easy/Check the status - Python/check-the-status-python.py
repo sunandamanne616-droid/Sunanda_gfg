4cb79231aeb747f60f8ea03c3dc69d95350df838
SUNANDA MANNE <sunandamanne616@gmail.com>
2026-06-25 23:56:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check the status - Python/check-the-status-python.py
@@ -0,0 +1,6 @@
+class Solution:
+    def checkStatus(self, a, b, flag):
+        if flag:
+            return a < 0 and b < 0
+        else:
+            return (a >= 0 and b < 0) or (a < 0 and b >= 0)
