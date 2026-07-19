6a908722a55581f6d6a7ce550ae917dfa7b9e455
SUNANDA MANNE <sunandamanne616@gmail.com>
2026-07-19 13:18:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.py
+++ Difficulty: Basic/Array Search/array-search.py
@@ -2,7 +2,6 @@ class Solution:
     def search(self, arr, x):
         for i in range(0,len(arr)):
             if arr[i]==x:
-                return i 
-                
+                return i
         return -1
         
