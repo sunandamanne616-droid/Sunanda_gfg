2900c6800449a25570939d4fd38749fa823c25c7
SUNANDA MANNE <sunandamanne616@gmail.com>
2026-07-24 13:33:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.py
+++ Difficulty: Medium/Majority Element/majority-element.py
@@ -1,13 +1,25 @@
 class Solution:
     def majorityElement(self, arr):
-        #code here
-        maj=len(arr)//2
-        freq={}
-        for i in arr:
-            freq[i]=freq.get(i,0)+1
-        for k,v in freq.items():
-            if v>maj:
-                return k
-            
+        candidate = None
+        count = 0
+
+        # Find candidate
+        for num in arr:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        # Verify candidate
+        count = 0
+        for num in arr:
+            if num == candidate:
+                count += 1
+
+        if count > len(arr) // 2:
+            return candidate
+
         return -1
-            
