a28b8ef84c1d6002e9fcd296459fe8486cefbca3
SUNANDA MANNE <sunandamanne616@gmail.com>
2026-07-29 20:07:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally Traversing a Matrix/spirally-traversing-a-matrix.py
@@ -0,0 +1,32 @@
+class Solution:
+    def spirallyTraverse(self, mat):
+        ans = []
+
+        top, bottom = 0, len(mat) - 1
+        left, right = 0, len(mat[0]) - 1
+
+        while top <= bottom and left <= right:
+
+            # Left to Right
+            for i in range(left, right + 1):
+                ans.append(mat[top][i])
+            top += 1
+
+            # Top to Bottom
+            for i in range(top, bottom + 1):
+                ans.append(mat[i][right])
+            right -= 1
+
+            # Right to Left
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    ans.append(mat[bottom][i])
+                bottom -= 1
+
+            # Bottom to Top
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    ans.append(mat[i][left])
+                left += 1
+
+        return ans
